0043995bf79d04dd6582fe58b619c2ca9ecfaf07
spotdown <apkspotmod@gmail.com>
2025-06-29 10:17:46 +0500

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,68 +1,37 @@
-import os
+# downloader.py
+import yt_dlp
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from yt_dlp import YoutubeDL
-from mutagen.easyid3 import EasyID3
-from mutagen.id3 import APIC, ID3
-import requests
-from dotenv import load_dotenv
+import os
 
-load_dotenv()
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-    client_id=os.getenv("SPOTIFY_CLIENT_ID"),
-    client_secret=os.getenv("SPOTIFY_CLIENT_SECRET")
+    client_id=SPOTIFY_CLIENT_ID,
+    client_secret=SPOTIFY_CLIENT_SECRET
 ))
 
+def download_spotify_track(url: str) -> str:
+    track_info = sp.track(url)
+    title = track_info["name"]
+    artist = track_info["artists"][0]["name"]
+    query = f"{title} {artist} audio"
 
-def search_youtube(query: str) -> str:
-    with YoutubeDL({'quiet': True}) as ydl:
-        result = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]
-        return result['webpage_url']
+    outtmpl = f"downloads/{title}-{artist}.mp3"
 
-
-def download_audio(url: str, output_path: str) -> str:
     ydl_opts = {
         'format': 'bestaudio/best',
-        'outtmpl': output_path,
+        'outtmpl': outtmpl,
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
-        }]
+            'preferredquality': '192',
+        }],
+        'quiet': True
     }
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-    return output_path.replace('.%(ext)s', '.mp3')
 
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([f"ytsearch1:{query}"])
 
-def tag_mp3(mp3_path: str, title: str, artist: str, album: str, cover_url: str):
-    audio = EasyID3(mp3_path)
-    audio['title'] = title
-    audio['artist'] = artist
-    audio['album'] = album
-    audio.save()
-
-    id3 = ID3(mp3_path)
-    cover_data = requests.get(cover_url).content
-    id3.add(APIC(encoding=3, mime='image/jpeg', type=3, desc='Cover', data=cover_data))
-    id3.save()
-
-
-def process_spotify_url(spotify_url: str) -> str:
-    track_id = spotify_url.split("/")[-1].split("?")[0]
-    track = sp.track(track_id)
-
-    title = track['name']
-    artist = track['artists'][0]['name']
-    album = track['album']['name']
-    cover_url = track['album']['images'][0]['url']
-
-    query = f"{title} {artist}"
-    youtube_url = search_youtube(query)
-
-    os.makedirs("downloads", exist_ok=True)
-    output_template = f"downloads/{title}_{artist}.%(ext)s"
-    mp3_path = download_audio(youtube_url, output_template)
-    tag_mp3(mp3_path, title, artist, album, cover_url)
-
-    return mp3_path
+    return outtmpl
